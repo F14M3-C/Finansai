781fe63a98c7b33dfaa545e61d9fae15bad2f083
F14M3 <augustinas142@gmail.com>
2026-03-19 15:18:08 +0200

    Add proxy to backend

--- public/vite.config.js
+++ public/vite.config.js
@@ -1,18 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+import "dotenv/config";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    watch: {
-      usePolling: true,
-    },
-  },
-})
+	plugins: [react(), tailwindcss()],
+	server: {
+		host: "0.0.0.0",
+		port: 5173,
+		watch: {
+			usePolling: true,
+		},
+		server: {
+			proxy: {
+				"/api": {
+					// eslint-disable-next-line no-undef
+					target: process.env.BACKEND_URL,
+					changeOrigin: true,
+					secure: false,
+				},
+			},
+		},
+	},
+});
