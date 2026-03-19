18e81f2ce942603495af31f5a27692031bee293b
eimantas-valantinas <eimantas.valantinas@stud.ktmc.lt>
2026-03-19 14:09:32 +0200

    veikiantis su prisma

--- server/src/routes/transcationRoutes.ts
+++ server/src/routes/transcationRoutes.ts
@@ -1,25 +1,9 @@
 import { Router, type Request, type Response } from 'express';
+import { prisma } from '../../lib/prisma';
 
 const router = Router();
 
-// pvz pridejimo
-
-type TransactionType = "INCOME" | "EXPENSE";
-
-interface Transaction {
-    id: number;
-    title: string;
-    amount: number;
-    type: TransactionType;
-    description: string | null;
-    accountId: number;
-    createdAt: Date;
-}
-
-let transactions: Transaction[] = [];
-let nextId = 1;
-
-router.post('/', (req: Request, res: Response) => {
+router.post('/', async (req: Request, res: Response) => {
     try {
         const { title, amount, type, description, accountId } = req.body;
 
@@ -27,22 +11,18 @@ router.post('/', (req: Request, res: Response) => {
             return res.status(400).json({ error: 'Missing required fields' });
         }
 
-        //sukurti nauja transakcija
+        // Sukurti naują transakciją DB per Prisma
+        const newTransaction = await prisma.transaction.create({
+            data: {
+                title,
+                amount: parseFloat(amount),
+                type: type, // "INCOME" l "EXPENSE" turi atitikti Enum'ą
+                description: description || null,
+                accountId: parseInt(accountId)
+            }
+        });
 
-        const newTransaction: Transaction = {
-            id: nextId++,
-            title,
-            amount: parseFloat(amount),
-            type: type as TransactionType,
-            description: description || null,
-            accountId: parseInt(accountId),
-            createdAt: new Date()
-        };
-
-        transactions.push(newTransaction);
-
-        // grazinti pavykusia transakcija
-
+        // Grąžinti pavykusią transakciją
         res.status(201).json({
             message: 'Operacija sėkmingai pridėta',
             data: newTransaction
@@ -53,9 +33,21 @@ router.post('/', (req: Request, res: Response) => {
         res.status(500).json({ error: 'Įvyko klaida pridedant operaciją' });
     }
 });
-// gauti visas transakcijas
-router.get('/', (req: Request, res: Response) => {
-    res.json({ data: transactions });
+
+// Gauti konkrečios paskyros (account) transakcijas
+router.get('/:accountId', async (req: Request, res: Response) => {
+    try {
+        const { accountId } = req.params;
+        const transactions = await prisma.transaction.findMany({
+            where: {
+                accountId: parseInt(accountId)
+            }
+        });
+        res.json({ data: transactions });
+    } catch (error) {
+        console.error('Error gaunant transakcijas:', error);
+        res.status(500).json({ error: 'Įvyko klaida gaunant operacijas' });
+    }
 });
 
 export default router;
